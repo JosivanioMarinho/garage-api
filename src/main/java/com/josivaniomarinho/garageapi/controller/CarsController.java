9d68a1754e39be7656b0e609633b22e7c3961731
JosivanioMarinho <Josivaniom69@gmail.com>
2021-01-04 13:27:02 -0300

    First commit in main

--- src/main/java/com/josivaniomarinho/garageapi/controller/CarsController.java
+++ src/main/java/com/josivaniomarinho/garageapi/controller/CarsController.java
@@ -1,6 +1,7 @@
 package com.josivaniomarinho.garageapi.controller;
 
 import jdk.jfr.Threshold;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/cars")
 public class CarsController {
 
+    @GetMapping
+    public String testeCars(){
+        return "Hello World!";
+    }
 }
